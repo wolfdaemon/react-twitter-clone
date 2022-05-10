9b2d5d681efee338589f4d8215f9b4089a20f751
unknown <Bambibi_1994@yahoo.com.vn>
2022-05-10 18:18:20 -0500

    Add Id as Global Variables

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import MyRoutes from './components/MyRoutes';
+
+//Global Variable but will not reset
+//Need to click F5 for the id to show up.
+window.id = localStorage.getItem('userId');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <h1>Quacker</h1>
-    {/* add id prop variable here */}
-    <MyRoutes/>
+    {/* <h2>@{window.id}</h2> */}
+    <MyRoutes />
   </React.StrictMode>
 );
